d83dd1312e5db33661bc1f39a95f3fb3a7b7bc43
Jechol Lee <mr.jechol@gmail.com>
2025-03-26 09:45:53 +0900

    igniter.new

---
+++ lib/ash_update_unload_check.ex
@@ -0,0 +1,18 @@
+defmodule AshUpdateUnloadCheck do
+  @moduledoc """
+  Documentation for `AshUpdateUnloadCheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshUpdateUnloadCheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_update_unload_check_test.exs
@@ -0,0 +1,8 @@
+defmodule AshUpdateUnloadCheckTest do
+  use ExUnit.Case
+  doctest AshUpdateUnloadCheck
+
+  test "greets the world" do
+    assert AshUpdateUnloadCheck.hello() == :world
+  end
+end
